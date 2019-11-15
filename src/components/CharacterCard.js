29673002bc814c2df0b23695cd484b07fcb1e3f7
ajmerr13 <oliverclosoff07@gmail.com>
2019-11-15 12:48:09 -0500

    mvp

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Card, CardSubtitle, CardBody, CardTitle, CardImg} from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+return (
+  
+    <Card>
+        <CardTitle>Name: {props.character.name}</CardTitle>
+        <CardImg src={props.character.image} />
+        <CardBody>
+        <CardSubtitle>ID: {props.character.id}</CardSubtitle>
+        <CardSubtitle>Gender: {props.character.gender}</CardSubtitle>
+        <CardSubtitle>Species: {props.character.species}</CardSubtitle>
+        </CardBody>
+      </Card>
+  
+)
 }
